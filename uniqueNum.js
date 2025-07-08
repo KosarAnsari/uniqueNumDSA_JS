1855b74c652a549fda73146845054092b103561e
KosarAnsari <kosaransari55@gmail.com>
2025-07-08 22:21:29 +0530

    Add files via upload

---
+++ uniqueNum.js
@@ -0,0 +1,21 @@
+//checking the unique numbers in an array
+function uniqueNum(array){
+    if(array.length != 0){
+        let i=0;
+        for(let j=1; j<array.length; j++){
+            if(array[i] !== array[j]){
+                i++;
+                array[i] = array[j];
+            }
+        }
+        return i+1;
+    }else{
+        throw new Error("Warning! Array is Empty");
+    }
+
+}
+
+let checkArr = uniqueNum([1,1,2,2,3,4,4,5,6,7,8,8]);
+console.log(checkArr);
+
+//linear time Complexity O(n)
